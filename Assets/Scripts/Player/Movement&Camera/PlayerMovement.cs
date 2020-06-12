2c4968ef08378ed1da4974f613dd8a93913a5111
alexandros <alex.anemogiannis@hotmail.com>
2020-06-12 15:29:51 +0300

    Umotion Installment / Gun Anim

--- Assets/Scripts/Player/Movement&Camera/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement&Camera/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private float movementSpeed = 3, sprintSpeed = 6, jumpHeight = 2, airSpeed = 1.5f;
 
     private Vector3 moveDirection = Vector3.zero;
-
+    private float gravity;
     public Vector2 GetInput() 
     {
         return new Vector2(moveDirection.x, moveDirection.z);
@@ -30,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
         
         TryGetComponent<CharacterController>(out characterController);
         TryGetComponent<PlayerAnimationmanager>(out animationManager);
+        gravity = Physics.gravity.y;
     }
 
     void Update()
@@ -39,9 +40,9 @@ public class PlayerMovement : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
         if (characterController.isGrounded)
         {
-            moveDirection.y = 0;
             moveDirection = transform.right * horizontal + vertical * transform.forward;
-            Debug.Log("Grounded");
+            moveDirection.y = 0;
+
             if (!InputManager.Instance.GetInputMethod().SprintKey())
             {
                 moveDirection *= movementSpeed;
@@ -50,16 +51,18 @@ public class PlayerMovement : MonoBehaviour
             {
                 moveDirection *= sprintSpeed;
             }
+            Debug.Log("Grounded");
             if (InputManager.Instance.GetInputMethod().JumpKey())
             {
-                moveDirection.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+                Debug.Log("Jump");
+                moveDirection.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
                 animationManager.SetJump("Jump");
             }
         }
         else
         {
-            moveDirection += (transform.right * horizontal * airSpeed + transform.forward * vertical * airSpeed + Physics.gravity.y * transform.up) * Time.deltaTime;
-            //Debug.Log("Not Grounded");
+            moveDirection += (transform.right * horizontal * airSpeed + transform.forward * vertical * airSpeed + transform.up * gravity) * Time.deltaTime;
+            Debug.Log("Not Grounded");
         }
         characterController.Move(moveDirection * Time.deltaTime);
         animationManager.SetMovement(new Vector2(horizontal, vertical));
